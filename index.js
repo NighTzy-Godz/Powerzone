a9f24d15ec535af1573b81c6d2b68fe2bb612820
Aser James <ajhubero16@gmail.com>
2021-08-23 11:48:44 +0800

    2nd

--- index.js
+++ index.js
@@ -8,23 +8,6 @@ headerToggler.addEventListener("click", () => {
 });
 
 // -----------------  BMI CALCULATOR ----------------------
-// BMI BACKGROUND
-let bmi = document.querySelector(".bmi");
-
-// WEIGHT
-let setWeight = document.querySelector("#weight").value;
-let newWeight = parseFloat(setWeight);
-
-// HEIGHT
-let setHeight = document.querySelector("#height").value;
-let newHeight = parseFloat(setHeight);
-
-// BMI ELEMTS TO CHANGE COLORS
-let indicator = document.querySelector("#indicate");
-let bmiDesc = document.querySelector(".bmiDesc");
-
-let bmihead = document.querySelector("#bmih1");
-let bmispan = document.querySelector("#bmispan");
 
 // TO AVOID LETTERS
 function letter(input) {
@@ -33,6 +16,24 @@ function letter(input) {
 }
 
 function getValue() {
+  // BMI BACKGROUND
+  let bmi = document.querySelector(".bmi");
+
+  // WEIGHT
+  let setWeight = document.querySelector("#weight").value;
+  let newWeight = parseFloat(setWeight);
+
+  // HEIGHT
+  let setHeight = document.querySelector("#height").value;
+  let newHeight = parseFloat(setHeight);
+
+  // BMI ELEMTS TO CHANGE COLORS
+  let indicator = document.querySelector("#indicate");
+  let bmiDesc = document.querySelector(".bmiDesc");
+
+  let bmihead = document.querySelector("#bmih1");
+  let bmispan = document.querySelector("#bmispan");
+
   // COMPUTATION
 
   let semiBmi = (newWeight / (newHeight * newHeight)) * 10000;
@@ -47,11 +48,16 @@ function getValue() {
     indicator.style.color = "white";
     bmihead.style.color = "white";
     bmispan.style.color = "white";
+    bmiDesc.style.color = "#121212";
   }
   // NORMAL
   if (newBmi > 18.5) {
     bmi.style.backgroundColor = "#c1e999";
     indicator.innerHTML = "STATUS: NORMAL";
+    indicator.style.color = "#1c2d3b";
+    bmihead.style.color = "#1c2d3b";
+    bmispan.style.color = "#1c2d3b";
+    bmiDesc.style.color = "#121212";
   }
   // OVERWEIGHT
   if (newBmi > 25) {
@@ -78,6 +84,9 @@ function getValue() {
 }
 
 function reset() {
+  let bmiDesc = document.querySelector(".bmiDesc");
+  let indicator = document.querySelector("#indicate");
+  let bmihead = document.querySelector("#bmih1");
   let bmi = document.querySelector(".bmi");
   let setWeight = (document.querySelector("#weight").value = "");
   let setHeight = (document.querySelector("#height").value = "");
@@ -86,6 +95,7 @@ function reset() {
   indicator.innerHTML = "YOUR STATUS";
   indicator.style.color = "var(--red)";
   bmihead.style.color = "var(--red)";
+  bmiDesc.style.color = "rgba(255, 255, 255, 0.795)";
 }
 
 // green #c1e999
